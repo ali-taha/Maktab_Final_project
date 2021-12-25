1e87d8293e4d2f7e76ba9c78c4e7a81b2e2a408b
ali-taha <alidtaha@gmail.com>
2021-12-25 23:58:31 +0330

    fix some bugs

--- shop/users/models.py
+++ shop/users/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15)
+
+    phone_number = models.CharField(max_length=15,unique=True)
+    is_seller = models.BooleanField(default=False,help_text=(
+            'Does the user have a Shop?'
+        ))
 
     REQUIRED_FIELDS = ['email', 'phone_number']
 
