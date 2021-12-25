0706590ec2ff8d9d02d030c2a0d967e4a82f32cb
ali-taha <alidtaha@gmail.com>
2021-12-25 19:12:03 +0330

    start project

---
+++ shop/users/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15)
+
+    REQUIRED_FIELDS = ['email', 'phone_number']
+
+
+    def __str__(self):
+        return self.username
