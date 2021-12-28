437327473dc7d90fabe2401155bdcca71150275e
ali-taha <alidtaha@gmail.com>
2021-12-29 01:15:30 +0330

    little ui change admin

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -22,3 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'Online_Shop Admin'
