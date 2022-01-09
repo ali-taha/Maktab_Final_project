0d6fc88a0fc138302fe6a01a03d1c93a5bf3543b
ali-taha <alidtaha@gmail.com>
2022-01-09 19:14:29 +0330

    fix settings

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -159,7 +159,8 @@ EMAIL_USE_TLS = True
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 SIMPLE_JWT = {
