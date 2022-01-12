7185a29817e4dc504551dadb061a1b8ee9a95e48
ali-taha <alidtaha@gmail.com>
2022-01-12 18:41:45 +0330

    fixing urls root

---
+++ shop/online_shop/urls_api.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from online_shop.views import StoreListApi, StoreTypeListApi, ProductListApi, BasketCreateApi, AddBasketItemApi, DeleteBasketItemApi, PayBasketApi, PaidBasketsApi
+
+
+
+urlpatterns = [
+
+    path('store/list',StoreListApi.as_view(), name='store_list_api'),
+    path('store/type',StoreTypeListApi.as_view(), name='store_type_list_api'),
+    path('<int:store>/product',ProductListApi.as_view(), name='product_list_api'),
+    path('basket/product/<int:product>',BasketCreateApi.as_view(), name='basket_create_api'),
+    path('basket-item/',AddBasketItemApi.as_view(), name='basket_add_item_api'),
+    path('basket-item/<int:id>',DeleteBasketItemApi.as_view(), name='delete_basket_item_api'),
+    path('basket2/<int:id>',PayBasketApi.as_view(), name='pay_basket_api'),
+    path('basket3/',PaidBasketsApi.as_view(), name='paid_baskets_api'),
+
+]
--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -17,13 +17,9 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from users.views import SignUpSeller, SignInSeller, LogoutView, SignUpApi, ProfileApi
-from online_shop.views import StoreListApi, StoreTypeListApi, ProductListApi, BasketCreateApi, AddBasketItemApi, DeleteBasketItemApi, PayBasketApi, PaidBasketsApi
-from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -38,31 +34,17 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns = [
-    path('',SignInSeller.as_view(), name='sign_in'),
-    path('sign-up',SignUpSeller.as_view() , name='sign_up'),
-    path('logout',LogoutView.as_view() , name='logout_view'),
+
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls')),
+    """    Sellers url's    """
+    path('', include('users.urls')),  
     path('dashboard/',include('online_shop.urls')),
-
-    path('api/sign-up',SignUpApi.as_view(), name='sign_up_api'),
-    path('api/sign-in/', TokenObtainPairView.as_view(), name='sign_in_api'),
-    path('api/sign-in/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/profile/<username>',ProfileApi.as_view(), name='profile_api'),
-
-    path('api/store/list',StoreListApi.as_view(), name='store_list_api'),
-    path('api/store/type',StoreTypeListApi.as_view(), name='store_type_list_api'),
-    path('api/<store>/product',ProductListApi.as_view(), name='product_list_api'),
-    path('api/basket/<product>',BasketCreateApi.as_view(), name='basket_create_api'),
-    path('api/basket-item/',AddBasketItemApi.as_view(), name='basket_add_item_api'),
-    path('api/basket-item/<int:id>',DeleteBasketItemApi.as_view(), name='delete_basket_item_api'),
-    path('api/basket2/<int:id>',PayBasketApi.as_view(), name='pay_basket_api'),
-    path('api/basket3/',PaidBasketsApi.as_view(), name='paid_baskets_api'),
-
-
-
+    """    Customers url's    """
+    path('user_api/v1/', include('users.urls_api')), # customers register, login url's
+    path('store_api/v1/', include('online_shop.urls_api')), # customers dashboard urls(api)
+    """    Swagger url's    """
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
